1e78ff39fc03889ee0997f3a99fe3f16bb6aa265
Sylvain Saucier <sylvain@sysau.com>
2024-08-25 15:10:54 -0700

    More beta progress for external module installation

--- additional_practrand.sh
+++ additional_practrand.sh
@@ -2,7 +2,18 @@
 #Run as root to download, compile and install the https://github.com/MartyMacGyver distribution of PractRand
 #Tested under macos and Linux.
 
+if ! command -v curl &> /dev/null
+then
+    if ! command -v wget &> /dev/null
+    then
+        echo "Please install curl or wget and try again."
+        exit 1
+    else
+    wget --output-document __extra__practrand.zip https://github.com/MartyMacGyver/PractRand/zipball/master
+    fi
+else
 curl -L -o __extra__practrand.zip https://github.com/MartyMacGyver/PractRand/zipball/master
+fi
 unzip __extra__practrand.zip
 cd MartyMacGyver-PractRand-6ae26d8/unix
 make
--- additional_testu01.sh
+++ additional_testu01.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
-#Run as root to install Dieharder in Linux Debian. May work in other apt based distributions.
+#Run as root to install TestU01 and build the stdin shims..
 
 mkdir _testu01_
 cd _testu01_
-basedir=`pwd`
-curl -OL http://simul.iro.umontreal.ca/testu01/TestU01.zip
-unzip TestU01.zip
+mkdir install_tree
+cd install_tree
+basedir=`.`
+cd ..
+
+if ! command -v curl &> /dev/null
+then
+    if ! command -v wget &> /dev/null
+    then
+        echo "Please install curl or wget and try again."
+        exit 1
+    else
+    wget --output-document __extra__testu01.zip http://simul.iro.umontreal.ca/testu01/TestU01.zip
+    fi
+else
+curl -L -o __extra__testu01.zip http://simul.iro.umontreal.ca/testu01/TestU01.zip
+fi
+
+unzip __extra__testu01.zip
 cd TestU01-1.2.3
 ./configure --prefix="$basedir"
 make -j 6
-make -j 6 install
-cd ..
-    mkdir lib-so
-    mv lib/*.so lib-so/.
+#make -j 6 install
+#cd ..
+#    mkdir lib-so
+#    mv lib/*.so lib-so/.
